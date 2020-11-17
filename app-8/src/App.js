f16170ff104913ca5a1e75109caf036b86d09f9e
GraidanChristensen <graidanc@gmail.com>
2020-11-16 17:15:00 -0700

    app 8 done

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      planet: {}
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://swapi.dev/api/planets/5/").then(response => {
+      this.setState({
+        planet: response.data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Planet: {this.state.planet.name}</h1>
+        <h1>Terrain: {this.state.planet.terrain}</h1>
+        <h1>Diameter: {this.state.planet.diameter}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
